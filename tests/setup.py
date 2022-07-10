4f5a9c1a11b9a0c06aa9667496e09bb9933c9075
Desiree <desireepr@nedbank.co.za>
2022-07-10 05:39:12 -0200

    first commit

---
+++ tests/setup.py
@@ -0,0 +1,14 @@
+from gettext import install
+from setuptools import setup, find_packages
+
+setup(
+    name='mypackage',
+    version='0.1',
+    packages=find_packages(exlude=['tests']),
+    license='MIT',
+    description='EDSA example python package',
+    long_description=open('README.md').read(),
+    install_requires=['numpy'],
+    url='https://github.com/<username>/<package-name>',
+    author='<Desiree>', email='<desireepr@nedbank.co.za>'
+)
